99528b5677715261ea65644080f7f56863d201dd
Daniman212 <physicsmaster212@yahoo.com>
2024-11-21 18:47:24 -0800

    added player rotation

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
         screen.fill((0, 0, 0))  # Fill the screen with black
         player.draw(screen)   # draw the player
         pygame.display.flip()  # Refresh the screen
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
