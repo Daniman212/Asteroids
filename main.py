41dd117b88c395a9aa8551fbc60afec0e17ee71e
Daniman212 <physicsmaster212@yahoo.com>
2024-11-21 18:04:11 -0800

    frame rate to 60 and circle class

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -12,6 +15,7 @@ def main():
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
         pygame.display.flip()  # Refresh the screen
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
